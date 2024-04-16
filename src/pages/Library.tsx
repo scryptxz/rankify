634c101e3078b84aa864464ba00a702c1f06679d
Renan <renanalmeida201612@gmail.com>
2024-04-15 22:43:05 -0300

    initial file upload

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,17 +1,43 @@
 // import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
 import ArtistsList from "../components/ArtistsList";
 import TracksList from "../components/TracksList";
 import Buttons from "../components/Buttons";
-import Logo from "../assets/imgs/logos/blastfm-logo-white.png"
+import Logo from "../assets/imgs/logos/blastfm-logo-white.png";
 import AlbumsList from "../components/AlbumsList";
 
 export default function Library() {
   const [type, setType] = useState<string>("artists");
+  const [inputFile, setInputFile] = useState<Event>();
+
+  function handleJSON(e: Event) {
+    console.log("ssssssssss");
+
+    const target = e.target as HTMLInputElement;
+    const file = target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = (e: ProgressEvent<FileReader>) => {
+        const data = e.target?.result as string;
+        console.log(data);
+        console.log(reader.result);
+      };
+
+      reader.readAsText(file);
+    } else {
+      alert("Error");
+    }
+  }
 
   return (
     <div className="min-h-screen py-8 bg-gradient-to-tl from-slate-700 to-blue-950 font-['Dosis'] font-semibold">
       <img src={Logo} alt="BlastFM logo" width={200} className="m-auto mb-8" />
+      <form action="" encType="multipart/form-data">
+        <input type="file" name="" id="" onChange={handleJSON} />
+        <button>Enviar</button>
+      </form>
       <div className="flex flex-col items-center m-auto gap-12 w-[60rem]">
         <Buttons type={type} setType={setType} />
         {/* <input
@@ -19,7 +45,13 @@ export default function Library() {
           className="w-full p-4 text-xl text-white bg-transparent border border-fuchsia-700 placeholder:text-fuchsia-700"
           placeholder="Search track/artists"
         /> */}
-        {type === "artists" ? <ArtistsList /> : type === "tracks" ? <TracksList /> : <AlbumsList />}
+        {type === "artists" ? (
+          <ArtistsList />
+        ) : type === "tracks" ? (
+          <TracksList />
+        ) : (
+          <AlbumsList />
+        )}
       </div>
     </div>
   );
